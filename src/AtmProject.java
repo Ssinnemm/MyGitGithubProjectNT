b2ae6eb484efd636c50117aff458a5695f3953d0
sinem <h.sinemayas@gmail.com>
2023-05-01 14:19:08 +0300

    nsb1

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -8,6 +8,8 @@ public class AtmProject {
         //version3
         System.out.println("Git öğrenmek çok kolay");
         //version4
+        System.out.println("Nesibe branchi kodlarını yazdı.");
+        //version nsb1
 
 
 
